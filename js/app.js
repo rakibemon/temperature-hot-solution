772e2676c94a4673e1e3e125615e3419a46923d9
rakibemon <rakibemon03@gmail.com>
2021-09-03 23:50:14 +0600

    weather solved

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const API_KEY=`04cc8152b129af08c8ab95583da0f5af`
+const searchTemp=()=>{
+    const city = document.getElementById('city-input').value;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric
+    `;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemp(data))
+
+};
+const setInnerText = (id,text)=>{
+    document.getElementById(id).innerText=text;
+}
+const displayTemp=(temp)=>{
+    setInnerText('city-name',temp.name)
+    setInnerText('temp',temp.main.temp)
+    setInnerText('condition', temp.weather[0].main)
+    document.getElementById('icon').setAttribute('src',`https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`);
+    console.log(temp)
+}
